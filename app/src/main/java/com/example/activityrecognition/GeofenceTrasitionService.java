a5ddcf96eebdcadec43678b830180224f7ac643b
skyler <konata.izumi20@gmail.com>
2019-10-24 02:38:19 -0400

    Add starting geofences broadcasts to track how long a phone is in the geofence.

--- app/src/main/java/com/example/activityrecognition/GeofenceTrasitionService.java
+++ app/src/main/java/com/example/activityrecognition/GeofenceTrasitionService.java
@@ -7,6 +7,7 @@ import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.os.Handler;
 import android.os.Looper;
@@ -25,6 +26,8 @@ import com.google.android.gms.location.GeofencingEvent;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.example.activityrecognition.MainActivity.loiteringDelayMs;
+
 public class GeofenceTrasitionService extends IntentService {
 
     private static final String TAG = GeofenceTrasitionService.class.getSimpleName();
@@ -33,6 +36,16 @@ public class GeofenceTrasitionService extends IntentService {
     public GeofenceTrasitionService() {
         super(TAG);
     }
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId)
+	{
+		IntentFilter filter = new IntentFilter();
+		filter.addAction("com.google.android.c2dm.intent.RECEIVE");
+		filter.addAction("com.google.android.c2dm.intent.REGISTRATION");
+		filter.addCategory("com.example.myapp");
+		return super.onStartCommand(intent,flags,startId);
+	}
 
     @Override
     protected void onHandleIntent(Intent intent) {
@@ -46,32 +59,30 @@ public class GeofenceTrasitionService extends IntentService {
 
         int geofenceTransition = geofencingEvent.getGeofenceTransition();
 
-
-        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER ||
-                geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
-
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_DWELL
+                || geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT
+                || geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
             // Get the geofences that were triggered. A single event can trigger multiple geofences.
             List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
 
-
-            String geofenceTransitionDetails = getGeofenceTrasitionDetails(
-                    geofenceTransition,
-                    triggeringGeofences
-            );
-
-            // Showing toast for location transition
-            Handler handler = new Handler(Looper.getMainLooper());
-            handler.post(new Runnable() {
-
-                @Override
-                public void run() {
-                    // Create a detail message with Geofences received
-                    //Toast.makeText(this,geofenceTransitionDetails,Toast.LENGTH_SHORT).show(); // buggg /:
-                }
-            });
-
-
-            Log.i(TAG, geofenceTransitionDetails);
+            if(geofenceTransition == Geofence.GEOFENCE_TRANSITION_DWELL) {
+                // start the timer
+                // Lets assume the first geofence listed in the return list is the one you are actually in.
+                Intent broadcastIntent = new Intent("com.example.activityrecognition.GeofenceTrasitionService");
+                broadcastIntent.putExtra("type",Geofence.GEOFENCE_TRANSITION_DWELL);
+                broadcastIntent.putExtra("place",triggeringGeofences.get(0).getRequestId());
+                broadcastIntent.putExtra("enterTime", System.currentTimeMillis() - loiteringDelayMs);
+                Log.i(TAG, "Dwelled in " + triggeringGeofences.get(0).getRequestId());
+                sendBroadcast(broadcastIntent);
+            } else if(geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+                // end the timer, save to main screen.
+				Intent broadcastIntent = new Intent("com.example.activityrecognition.GeofenceTrasitionService");
+				broadcastIntent.putExtra("type",Geofence.GEOFENCE_TRANSITION_EXIT);
+                broadcastIntent.putExtra("place",triggeringGeofences.get(0).getRequestId());
+                broadcastIntent.putExtra("exitTime", System.currentTimeMillis());
+				Log.i(TAG, "Exited " + triggeringGeofences.get(0).getRequestId());
+                sendBroadcast(broadcastIntent);
+            }
         } else {
             // Log the error.
             Log.e(TAG, "Error in the Geofence TransitionDetails");
--- app/src/main/java/com/example/activityrecognition/MainActivity.java
+++ app/src/main/java/com/example/activityrecognition/MainActivity.java
@@ -11,12 +11,14 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -46,11 +48,13 @@ import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements
 		GoogleApiClient.ConnectionCallbacks,
@@ -58,7 +62,7 @@ public class MainActivity extends AppCompatActivity implements
 		OnMapReadyCallback,
 		ResultCallback<Status>,
 		SensorEventListener,
-		StepCounterContainer {
+		StepCounterContainer{
 
 
 	private GoogleApiClient mApiClient;
@@ -69,10 +73,14 @@ public class MainActivity extends AppCompatActivity implements
 	private HashMap<String, LatLng> geofenceInfo;
 	private ArrayList<Geofence> geofences;
 	private PendingIntent geoFencePendingIntent;
+	private Intent geofenceIntent;
 	private static final int MAP_CAMERA_ZOOM = 17;
 	private final int GEOFENCE_REQ_CODE = 0;
 	private static final float GEOFENCE_RADIUS = 25.0f; // in meters
-	private static final int loiteringDelayMs = 15000;
+	public static final int loiteringDelayMs = 15000;
+	private String currGeofenceID = "";
+	private long lastEnterTime = 0;
+	private BroadcastReceiver broadcastReceiver;
 
 	private static ImageView detectedActivityImageView;
 	private static TextView activityTextView;
@@ -95,9 +103,6 @@ public class MainActivity extends AppCompatActivity implements
 			geofenceInfo.put("Fuller", new LatLng(42.274852, -71.806690));
 			geofenceInfo.put("Library", new LatLng(42.274292, -71.806641));
 			startGeofence();
-		} else {
-			Toast toast = Toast.makeText(getApplicationContext(), "No Permissions for geofences", Toast.LENGTH_LONG);
-			toast.show();
 		}
 
 		SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
@@ -108,13 +113,13 @@ public class MainActivity extends AppCompatActivity implements
 
 		mapFragment.getMapAsync(this);
 
-		mApiClient = new GoogleApiClient.Builder(this)
-				.addApi(ActivityRecognition.API)
-				.addConnectionCallbacks(this)
-				.addOnConnectionFailedListener(this)
-				.build();
-
-		mApiClient.connect();
+//		mApiClient = new GoogleApiClient.Builder(this)
+//				.addApi(ActivityRecognition.API)
+//				.addConnectionCallbacks(this)
+//				.addOnConnectionFailedListener(this)
+//				.build();
+//
+//		mApiClient.connect();
 
 
 		// Sensor init stuff for step counting
@@ -149,7 +154,6 @@ public class MainActivity extends AppCompatActivity implements
 	/* +++++++++++++++++++   GeoFence start +++++++++++++++++++++++ */
 
 	// Start Geofence creation process
-	// Start one for fuller, one for library.
 	private void startGeofence() {
 		Log.i(TAG, "startGeofence()");
 		geoClient = LocationServices.getGeofencingClient(this);
@@ -162,8 +166,48 @@ public class MainActivity extends AppCompatActivity implements
 				@Override
 				public void onSuccess(Void aVoid) {
 					// moved drawing geofences to onMapReady
+					Log.i(TAG, "Added geofences successfully.");
+				}
+			})
+			.addOnFailureListener(this, new OnFailureListener() {
+				@Override
+				public void onFailure(@NonNull Exception e) {
+					Log.i(TAG, "Failed to add geofences.");
+					Log.i(TAG, e.getMessage());
 				}
 			});
+
+		broadcastReceiver = new BroadcastReceiver() {
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				String broadcast = intent.getAction();
+				if(broadcast.equalsIgnoreCase("com.example.activityrecognition.GeofenceTrasitionService")) {
+					String place = intent.getStringExtra("place");
+					int type = intent.getIntExtra("type", -1);
+					switch(type) {
+						case Geofence.GEOFENCE_TRANSITION_DWELL:
+							long enterTime = intent.getLongExtra("enterTime",-1);
+							if (enterTime > 0) {
+								currGeofenceID = place;
+								lastEnterTime = enterTime;
+							}
+							break;
+						case Geofence.GEOFENCE_TRANSITION_EXIT:
+							long exitTime = intent.getLongExtra("exitTime",-1);
+							if (exitTime > 0 && place.equals(currGeofenceID)) {
+								float duration = (exitTime - lastEnterTime) / 1000f;
+								String msg = String.format(Locale.US, "You have been in %s geofence for %.2f seconds", place, duration);
+								Toast toast = Toast.makeText(getApplicationContext(), msg,Toast.LENGTH_LONG);
+								toast.show();
+							}
+							currGeofenceID = "";
+							break;
+					}
+				}
+			}
+		};
+		IntentFilter filter = new IntentFilter("com.example.activityrecognition.GeofenceTrasitionService");
+		registerReceiver(broadcastReceiver, filter);
 	}
 
 	// Create a Geofence --step1--
@@ -194,9 +238,10 @@ public class MainActivity extends AppCompatActivity implements
 		if (geoFencePendingIntent != null)
 			return geoFencePendingIntent;
 
-		Intent intent = new Intent(this, GeofenceTrasitionService.class);
-		return PendingIntent.getService(
-				this, GEOFENCE_REQ_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+		geofenceIntent = new Intent(this, GeofenceTrasitionService.class);
+		geoFencePendingIntent = PendingIntent.getService(
+				this, GEOFENCE_REQ_CODE, geofenceIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+		return geoFencePendingIntent;
 	}
 
 	// +++++++++++++++++++ GeoFence end +++++++++++++++++++++++
@@ -255,10 +300,20 @@ public class MainActivity extends AppCompatActivity implements
 		MapsInitializer.initialize(getApplicationContext());
 		map = googleMap;
 		map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+		map.setMyLocationEnabled(true);
+
 
 		drawGeofences();
 		latlng = new LatLng(42.2742608, -71.8066728);
 		map.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, MAP_CAMERA_ZOOM));
+
+		mApiClient = new GoogleApiClient.Builder(this)
+				.addApi(ActivityRecognition.API)
+				.addConnectionCallbacks(this)
+				.addOnConnectionFailedListener(this)
+				.build();
+
+		mApiClient.connect();
 	}
 
 	// Draw Geofence circle on GoogleMap
